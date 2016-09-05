9b93e1b36916f5f10e8e71816ccb1e87e8dff711
Publicover <jameslpublicover@gmail.com>
2016-09-05 16:13:25 -0400

    made my first func that calculates average of Ints in an array

--- playgroundScratch.playground/Contents.swift
+++ playgroundScratch.playground/Contents.swift
@@ -49,38 +49,100 @@
     /// print("Everything tastes good in a soup.")
 /// }
 
-let interestingNumbers = [
-    "Primes": [2, 3, 5, 7, 11, 13],
-    "Fibonacci": [1, 1, 2, 3, 5, 8],
-    "Square": [1, 4, 9, 16, 25],
-]
+/// let interestingNumbers = [
+///     "Primes": [2, 3, 5, 7, 11, 13],
+///     "Fibonacci": [1, 1, 2, 3, 5, 8],
+///     "Square": [1, 4, 9, 16, 25],
+/// ]
 
-var largest = 0
-var previousLargest = 0
+/// var largest = 0
+/// var previousLargest = 0
 
-for (kind, numbers) in interestingNumbers {
+/// for (kind, numbers) in interestingNumbers {
+    
+///     for number in numbers {
+///         if number > largest {
+///             previousLargest = largest
+///             largest = number
+///         }
+///     }
+/// }
+
+/// print(largest)
+/// print(previousLargest)
+
+/// var n = 2
+/// while n < 100{
+///     n = n * 2
+/// }
+/// print(n)
+
+
+
+
+
+
+/// func greet(name: String, day: String) -> String {
+///     return "Hello, \(name), today is \(day)."
+/// }
+
+/// greet("Bob", day: "Tuesday")
+
+/// func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    
+///     var min = scores[0]
+///     var max = scores[0]
+///     var sum = scores[0]
+    
+///     for score in scores {
+///         if score > max {
+///             max = score
+///         } else if score < min {
+///             min = score
+///         }
+        
+///         sum += score
+        
+///     }
+    
+///     return (min, max, sum)
+/// }
+
+/// let statistics = calculateStatistics([5, 3, 100, 3, 9])
+/// print(statistics.sum)
+/// print(statistics.2)
+
+func sumOf(numbers: [Int]) -> Int {
+    var sum = 0
     
     for number in numbers {
-        if number > largest {
-            previousLargest = largest
-            largest = number
-        }
+        sum += number
     }
+    
+    return sum
 }
 
-print(largest)
-print(previousLargest)
+var numArray = [1, 2, 3, 4, 5]
 
-var n = 2
-while n < 100{
-    n = n * 2
+sumOf(numArray)
+sumOf([42, 597, 12])
+sumOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+func avgOfArray(numbers: [Int]) -> Int {
+    var sum = 0
+    var average = 0
+    
+    for number in numbers {
+        sum += number
+        
+    average = sum / numbers.count
+        
+    }
+    
+    return average
 }
-print(n)
 
-
-
-
-
+avgOfArray(numArray)
 
 
 
